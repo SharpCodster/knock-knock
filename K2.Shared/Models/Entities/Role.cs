c21e456e01651b7342152740b25215abd9634cfe
Marco Fusilli <m.fusilli@outlook.com>
2022-12-23 10:16:51 +0100

    WIP don't remember

--- K2.Shared/Models/Entities/Role.cs
+++ K2.Shared/Models/Entities/Role.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using K2.Shared.Interfaces.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace K2.Shared.Models.Entities
 {
-    public class Role
+    public class Role : BaseEntity
     {
+        [Column(Order = 1)]
+        public string Name { get; set; }
+        [Column(Order = 2)]
+        public string Descritpion { get; set; }
+        [Column(Order = 3)]
+        public string ClinetId { get; set; }
+        [Column(Order = 4)]
+        public string ClientSecret { get; set; }
+        [Column(Order = 5)]
+        public bool IsActive { get; set; } = true;
     }
+}
 }
